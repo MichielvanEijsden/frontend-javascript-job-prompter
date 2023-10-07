1ccc7d3f618ef7483f8d2979b5e96ff7c6d058e8
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2023-10-07 16:18:47 +0200

    mijn uitwerkingen

---
+++ scripts/opdracht-3.js
@@ -0,0 +1,23 @@
+const userInput1 = prompt(`Je koos marketing. Over welke functie wil je meer weten? Voer een getal tussen 0 en 4. 0: ${departments.marketing.jobs[0].title},  1: ${departments.marketing.jobs[1].title},  2: ${departments.marketing.jobs[2].title},  3: ${departments.marketing.jobs[3].title}, 4: ${departments.marketing.jobs[4].title}`)
+console.log(userInput1)
+
+switch (userInput1){
+    case "0":
+        console.log("Je koos :",departments.marketing.jobs[0].title,",Een uitdagende rol !", departments.marketing.jobs[0].description);
+        break
+    case "1":
+        console.log("Je koos :",departments.marketing.jobs[1].title,",Een uitdagende rol !", departments.marketing.jobs[1].description);
+        break
+    case "2":
+        console.log("Je koos :",departments.marketing.jobs[2].title,",Een uitdagende rol !", departments.marketing.jobs[2].description);
+        break
+    case "3":
+        console.log("Je koos :",departments.marketing.jobs[3].title,",Een uitdagende rol !", departments.marketing.jobs[3].description);
+        break
+    case "4":
+        console.log("Je koos :",departments.marketing.jobs[4].title,",Een uitdagende rol !", departments.marketing.jobs[4].description);
+        break
+    default:
+        console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.");
+        break
+}
